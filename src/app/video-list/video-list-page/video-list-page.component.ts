c94014d989c8166c951cdaae14b0727bfbb966ea
Marvin Deuschle <marvdeus@gmail.com>
2021-01-26 19:47:53 +0100

    started background-image

--- src/app/video-list/video-list-page/video-list-page.component.ts
+++ src/app/video-list/video-list-page/video-list-page.component.ts
@@ -1,22 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Level } from 'src/app/models/level';
-import { Video } from 'src/app/models/video';
 import { LevelService } from 'src/app/services/level.service';
-import { VideoListService } from '../services/video-list.service';
 
 @Component({
   selector: 'app-video-list-page',
   templateUrl: './video-list-page.component.html',
-  styleUrls: ['./video-list-page.component.scss']
+  styleUrls: ['./video-list-page.component.scss'],
 })
 export class VideoListPageComponent implements OnInit {
-
   levels: Level[] = [];
 
-  constructor(private levelService: LevelService) { }
+  constructor(private levelService: LevelService) {}
 
   ngOnInit(): void {
     this.levels = this.levelService.getLevels();
   }
-
 }
